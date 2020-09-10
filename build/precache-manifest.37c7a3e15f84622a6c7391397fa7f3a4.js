35e91b5119d58f34ae7ee90cad958ba0b389ca79
Sami Safadi <samisaf@gmail.com>
2020-09-10 15:01:48 -0400

    Fixes relative path

---
+++ build/precache-manifest.37c7a3e15f84622a6c7391397fa7f3a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a37e6e070290d600c98df9ceef6959",
+    "url": "./index.html"
+  },
+  {
+    "revision": "619da3f794d3d430a6cb",
+    "url": "./static/css/main.a584fb0e.chunk.css"
+  },
+  {
+    "revision": "79d0e6753a6c8bcee1b4",
+    "url": "./static/js/2.e554d416.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e554d416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "619da3f794d3d430a6cb",
+    "url": "./static/js/main.9ea4f923.chunk.js"
+  },
+  {
+    "revision": "03575165036da8444cf4",
+    "url": "./static/js/runtime-main.8f28b43c.js"
+  }
+]);
